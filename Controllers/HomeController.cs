8ec41c3a0651af441008438a9f9f5f44e0032830
Simon Gough <simon@oncebyten.com>
2015-01-09 16:02:09 +0000

    1
    1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Client1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "";
 
             return View();
         }
